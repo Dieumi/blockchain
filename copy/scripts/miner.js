79284f7a2578fdca217324f589f43dd71834cd76
Dieumi <oxeyo@hotmail.fr>
2021-01-04 10:29:51 -0500

    miner.js

---
+++ copy/scripts/miner.js
@@ -0,0 +1,20 @@
+console.log("PATATE2")
+var mining_threads = 1
+console.log("PATATE1")
+function checkWork() {
+    console.log("PATATE0")
+    if (eth.getBlock("pending").transactions.length > 0) {
+        if (eth.mining) return;
+        console.log("== Pending transactions! Mining...");
+        miner.start(mining_threads);
+    } else {
+        miner.stop();
+        console.log("== No transactions! Mining stopped.");
+    }
+}
+
+
+eth.subscribe("latest", function(err, block) { checkWork(); });
+eth.subscribe("pending", function(err, block) { checkWork(); });
+
+checkWork();
